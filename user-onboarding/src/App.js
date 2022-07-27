e8d0eec3f096581538ca5b82b31d6cc2b7c542c4
Alexander Monteiro <alexandermonteiro@Priscilas-MBP.attlocal.net>
2022-07-27 13:43:32 -0700

    tasks 1-2

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+import Form from "./Components/Form";
+
+const initialFormValues = {
+  username: "",
+  email: "",
+  password: "",
+  checked: false,
+};
 
 function App() {
+  const [formValues, setFormValues] = useState(initialFormValues);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form values={formValues} />
     </div>
   );
 }
